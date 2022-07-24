97549adccb25e1a1aea43104a7ce5516e8fdd972
dong-geon-Lee <ch9716@naver.com>
2022-07-24 09:41:52 +0900

    header components add

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,32 +1,41 @@
 import Image from "next/image";
+import { SearchIcon } from "@heroicons/react/outline";
 
 export default function Header() {
   return (
-    <div>
+    <div className="flex items-center justify-between max-w-6xl">
       {/* Left */}
-      <div className="flex items-center justify-between max-w-6xl">
-        <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
-          <Image
-            src="https://1iwky71fbek0h7wy43b8a0i2-wpengine.netdna-ssl.com/wp-content/uploads/2021/03/Instagram-Text.png"
-            layout="fill"
-            className="object-contain"
-          />
-        </div>
+      <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
+        <Image
+          src="https://1iwky71fbek0h7wy43b8a0i2-wpengine.netdna-ssl.com/wp-content/uploads/2021/03/Instagram-Text.png"
+          layout="fill"
+          className="object-contain"
+        />
+      </div>
 
-        <div className="cursor-pointer h-24 w-10 relative lg:hidden">
-          <Image
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/640px-Instagram_logo_2022.svg.png"
-            layout="fill"
-            className="object-contain"
-          />
-        </div>
-
-        <h1>Right sides</h1>
+      <div className="cursor-pointer h-24 w-10 relative lg:hidden">
+        <Image
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/640px-Instagram_logo_2022.svg.png"
+          layout="fill"
+          className="object-contain"
+        />
       </div>
 
       {/* Middle */}
+      <div className="relative mt-1">
+        <div className="absolute top-2 left-2">
+          <SearchIcon className="h-5 text-gray-500" />
+        </div>
+        <input
+          type="text"
+          placeholder="Search"
+          className="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md"
+        />
+      </div>
 
       {/* Right */}
+
+      <h1>Right sides</h1>
     </div>
   );
 }
