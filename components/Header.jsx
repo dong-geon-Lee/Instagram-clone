c325bdb4be1aafd3ba660ae4a432557ceb99a783
dong-geon-Lee <ch9716@naver.com>
2022-07-24 09:57:53 +0900

    right side of the header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
-import { SearchIcon } from "@heroicons/react/outline";
+import { HomeIcon, SearchIcon } from "@heroicons/react/solid";
+import { PlusCircleIcon } from "@heroicons/react/outline";
 
 export default function Header() {
   return (
-    <div className="flex items-center justify-between max-w-6xl">
+    <div className="flex items-center justify-between max-w-6xl mx-4 xl:mx-auto">
       {/* Left */}
       <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
         <Image
@@ -35,7 +36,15 @@ export default function Header() {
 
       {/* Right */}
 
-      <h1>Right sides</h1>
+      <div className="flex space-x-4 items-center">
+        <HomeIcon className="hidden md:inline-flex h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
+        <PlusCircleIcon className="h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
+        <img
+          src="https://newsimg.sedaily.com/2020/05/28/1Z2XCSYAIX_1.jpg"
+          alt="user-image"
+          className="h-10 w-10 rounded-full cursor-pointer"
+        />
+      </div>
     </div>
   );
 }
